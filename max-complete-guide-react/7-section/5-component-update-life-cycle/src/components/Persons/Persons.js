f4371f55bcbcf092b8f7c7171957a73fec2cd238
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-05-23 21:47:45 -0700

    some practice on react

--- max-complete-guide-react/7-section/5-component-update-life-cycle/src/components/Persons/Persons.js
+++ max-complete-guide-react/7-section/5-component-update-life-cycle/src/components/Persons/Persons.js
@@ -3,14 +3,7 @@ import React, { Component } from 'react';
 import Person from './Person/Person';
 
 class Persons extends Component {
-  // static getDerivedStateFromProps(props, state) {
-  //   console.log('[Persons.js] getDerivedStateFromProps');
-  //   return state;
-  // }
 
-  // componentWillReceiveProps(props) {
-  //   console.log('[Persons.js] componentWillReceiveProps', props);
-  // }
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log('[Persons.js] shouldComponentUpdate');
