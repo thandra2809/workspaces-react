b6badc0dc26fca8bd5383ae8f07ae97202cd74a7
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-08-08 21:37:55 -0700

    added new changes

---
+++ andre-complete-react/crwn-clothing/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {HomePage} from './homepage.component';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <HomePage></HomePage>
+        </div>
+    );
+}
+
+export default App;
