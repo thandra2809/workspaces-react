9924452c9cfb3ce0f6432f6ecdd98ab2a540a7a7
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-08-11 00:19:16 -0700

    added new changes

---
+++ andre-complete-react/crwn-clothing/config/firebase-config.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyADDshZe3n2lX4CEdvJk6bahaTds5UAplY",
+    authDomain: "crwn-db-57341.firebaseapp.com",
+    databaseURL: "https://crwn-db-57341.firebaseio.com",
+    projectId: "crwn-db-57341",
+    storageBucket: "crwn-db-57341.appspot.com",
+    messagingSenderId: "77239174760",
+    appId: "1:77239174760:web:af9e8af8e092404a689ddd",
+    measurementId: "G-BF96F2XK37"
+};
