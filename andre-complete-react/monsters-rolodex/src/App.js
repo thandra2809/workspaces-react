9975609df69967776889ffcb7c3d918f30b2b574
Srinivasa Rao THANDRA <thandra82@gmail.com>
2021-01-04 23:39:29 -0800

    added new changes

--- andre-complete-react/monsters-rolodex/src/App.js
+++ andre-complete-react/monsters-rolodex/src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {CardList} from "./components/card-list/card-list.component";
 import {SearchBox} from "./components/search-box/search-box.componet";
