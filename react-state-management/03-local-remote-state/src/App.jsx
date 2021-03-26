38d3832e431c190c6c40cd0d443144a542b8a31b
Srinivasa Rao THANDRA <thandra2809>
2021-03-25 23:33:48 -0700

    Added new Changes

--- react-state-management/03-local-remote-state/src/App.jsx
+++ react-state-management/03-local-remote-state/src/App.jsx
@@ -2,23 +2,17 @@ import React, {useState, useEffect} from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import {getProducts} from "./services/productService";
 import Spinner from "./Spinner";
+import useFetch from "./services/useFetch";
 
 
 export default function App() {
 
     const [size, setSize] = useState("")
-    const [products, setProducts] = useState([])
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        getProducts("shoes")
-            .then((response) => setProducts(response))
-            .catch((e)=>setError(e))
-            .finally(()=> setLoading(false));
-    }, [])
+    const { data: products, loading, error } = useFetch(
+        "products?category=shoes"
+    );
 
     function renderProduct(p) {
         return (
@@ -32,9 +26,11 @@ export default function App() {
         );
     }
 
-    const filteredProducts = size ? products.filter((p) => p.skus.find((s) => s.size === parseInt(size))) : products;
+    const filteredProducts = size ? products
+        .filter((p) => p.skus.find((s) => s.size === parseInt(size))) : products;
 
     if (error) throw error
+
     if(loading) return <Spinner/>
 
     return (
